ab2916afaae3fded1e951e6d43251563e129dae4
Travis Geiselbrecht <geist@foobox.com>
2017-12-20 19:15:07 -0800

    WIP monitor

--- rom_c/main.c
+++ rom_c/main.c
@@ -35,12 +35,13 @@ char *readline() {
     static char linebuf[256];
     uint8_t pos = 0;
 
-    // read a line of characters
+    // read a line of characters, skipping unprintable
     while (pos < 255) {
         int c = getchar();
         switch (c) {
             case '\r':
             case '\n':
+                putchar('\n');
                 goto done;
             case 0x20 ... 0x7f:
                 linebuf[pos++] = c;
@@ -55,45 +56,89 @@ done:
     return linebuf;
 }
 
+uint16_t parse_hex_digit(const char *str, char *len_found) {
+    uint16_t val = 0;
+    *len_found = 0;
+
+    while (*str != '\0') {
+        if (*str >= '0' && *str <= '9') {
+            (*len_found)++;
+            val <<= 4;
+            val += *str - '0';
+        } else if (*str >= 'A' && *str <= 'F') {
+            (*len_found)++;
+            val <<= 4;
+            val += *str - 'A' + 0xa;
+        } else {
+            break;
+        }
+        str++;
+    }
+
+    return val;
+}
+
+static uint16_t dump_memory(uint16_t addr) {
+    uint8_t *ptr = (uint8_t*)addr;
+
+    char i;
+    for (i = 0; i < 16; i++) {
+        put_hex16(addr); puts(": ");
+        char j;
+        for (j = 0; j < 16; j++) {
+            put_hex8(*ptr++);
+            putchar(' ');
+        }
+        addr += j;
+        putchar('\n');
+    }
+
+    return 0x100;
+}
 
 static void do_command() {
     puts("\n# ");
 
-    // read the command char
+    // read the line
     char *line = readline();
-    puts(line);
-#if 0
+
+    // trim on the left
+    while (*line == ' ')
+        line++;
+
+    // look for a command character next
     char cmd;
     bool readmem;
-    for (;;) {
-        int c = getchar();
-        putchar(c);
+    switch (line[0]) {
+        case 'M': // inspect memory
+            cmd = line[0];
+            line++;
+            readmem = true;
+            break;
+        default:
+            puts("\nunrecognized command");
+            return;
+    }
 
-        switch (c) {
-            case 'M': // inspect memory
-                cmd = c;
-                readmem = true;
-                break;
-            case '\r':
-            case '\n':
-                return;
-            case ' ':
-                continue;
-            default:
-                puts("\nunrecognized command");
-                return;
+    // parse a memory address
+    static uint16_t lastmem;
+    uint16_t mem = 0;
+    if (readmem) {
+        char found;
+        mem = parse_hex_digit(line, &found);
+        line += found;
+        if (found > 0) {
+            lastmem = mem;
+        } else {
+            mem = lastmem;
         }
     }
 
-    if (readmem) {
-        uintptr_t mem = 0;
-
-        int c = getchar();
-        putchar(c);
+    switch (cmd) {
+        case 'M': // inspect memory
+            lastmem += dump_memory(mem);
+            break;
     }
-#endif
-
-
 }
 
 int _main() {
